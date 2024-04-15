625a3ab9ae978e5c21b9674214fe9341585b7696
Arghya <arghyaghosho48@gmail.com>
2024-04-15 18:20:10 +0530

    redifine routers

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect("mongodb+srv://arghyaghosh048:iamarghya007@cluster0.uwexszt.mongodb.net/chatApp");
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.bold);
+    process.exit(1); // Exit with a non-zero status code to indicate an error
+  }
+};
+
+
+module.exports = connectDB;
+
